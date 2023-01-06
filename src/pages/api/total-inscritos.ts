8b581b06d0aecac8631b49d21e338ab115774c37
pablowinck <pablowinck123@gmail.com>
2023-01-05 22:11:00 -0300

    endpoint total-inscritos

---
+++ src/pages/api/total-inscritos.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { api } from "../../services/api";
+
+export default async function handler(_: NextApiRequest, res: NextApiResponse) {
+  const ingressantes = await api.get("/ingressantes").then((res) => res.data);
+  return res.status(200).json({ result: ingressantes.length });
+}
