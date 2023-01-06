21ec67752e91ec8baea5ae4e1b6fbd4bdba96be7
pablowinck <pablowinck123@gmail.com>
2023-01-05 22:10:44 -0300

    tipagem ingressante

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,11 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "http://localhost:8080",
 });
+
+export type Ingressante = {
+  id: number;
+  nome: string;
+  curso: string;
+  estado: string;
+  cidade: string;
+};
