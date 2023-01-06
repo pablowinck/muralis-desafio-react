9c14d7d0f832e9ef5659019f04795decdad5a809
pablowinck <pablowinck123@gmail.com>
2023-01-05 22:10:53 -0300

    endpoint porcentagem-curso

---
+++ src/pages/api/porcentagem-curso.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { Ingressante, api } from "../../services/api";
+
+export default async function handler(_: NextApiRequest, res: NextApiResponse) {
+  const ingressantes = await api
+    .get<Ingressante[]>("/ingressantes")
+    .then((res) => res.data);
+
+  const totalPorCurso = ingressantes.reduce((acc, ingressante) => {
+    if (acc[ingressante.curso]) {
+      acc[ingressante.curso] += 1;
+    } else {
+      acc[ingressante.curso] = 1;
+    }
+    return acc;
+  }, {} as Record<string, number>);
+
+  const porcentagemPorCurso = Object.entries(totalPorCurso).reduce(
+    (acc, [curso, total]) => {
+      acc[curso] = ((total / ingressantes.length) * 100).toFixed(2);
+      return acc;
+    },
+    {} as Record<string, string>
+  );
+
+  return res.status(200).json(porcentagemPorCurso);
+}
